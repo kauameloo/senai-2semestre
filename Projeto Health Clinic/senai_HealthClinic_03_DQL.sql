c2758f1e00bc55d1f73a83a37b7e3d3b97c11dcc
Kauã Rodrigues <kaua.rodrigues2620@gmail.com>
2023-08-21 17:21:18 -0300

    Inicio do API

--- Projeto Health Clinic/senai_HealthClinic_03_DQL.sql
+++ Projeto Health Clinic/senai_HealthClinic_03_DQL.sql
@@ -5,6 +5,24 @@ GO;
 
 EXEC TodosMedico
 
+create function BuscaMedico
+(
+	@Especialidade varchar(100)
+)
+returns table
+as
+return
+(
+	select MedicoUsuario.Nome as Médico, 
+	Especialidade.TituloEspecialidade as Especialidade
+	from Especialidade
+	inner join Medico on Medico.IdEspecialidade = Especialidade.IdEspecialidade
+	inner join Usuario as MedicoUsuario on Medico.IdUsuario = MedicoUsuario.IdUsuario
+	where Especialidade.TituloEspecialidade = @Especialidade
+);
+
+select * from BuscaMedico('Cardiologista')
+
 
 SELECT * FROM TipoUsuario
 SELECT * FROM Clinica
@@ -30,6 +48,6 @@ from Consulta
 inner join Paciente on Consulta.IdPaciente = Paciente.IdPaciente
 inner join Medico on Consulta.IdMedico = Medico.IdMedico
 inner join Usuario on Paciente.IdUsuario = Usuario.IdUsuario
+INNER JOIN Usuario AS MedicoUsuario ON Medico.IdUsuario = MedicoUsuario.IdUsuario
 inner join Clinica on Usuario.IdClinica = Clinica.IdClinica
 inner join Especialidade on Medico.IdEspecialidade = Especialidade.IdEspecialidade
-INNER JOIN Usuario AS MedicoUsuario ON Medico.IdUsuario = MedicoUsuario.IdUsuario
---
+++ api_tarde/webapi.filmes.tarde/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,53 @@
+﻿using webapi.filmes.tarde.Domains;
+
+namespace webapi.filmes.tarde.Interfaces
+{
+    /// <summary>
+    /// Interface responsável pelo repositório GeneroRepository
+    /// Define os métodos que serão implementados pelo repositório
+    /// </summary>
+    public interface IGeneroRepository
+    {
+        //CRUD
+
+        //TipoDeRetorno NomeMetodo(TipoParametro Parametro)
+
+        /// <summary>
+        /// Cadastrar novo Genero
+        /// </summary>
+        /// <param name="novoGenero">Objeto que será cadastrado</param>
+        void Cadastrar(GeneroDomain novoGenero);
+
+        /// <summary>
+        /// Retornar todos os gêneros cadastrados
+        /// </summary>
+        /// <returns>Uma lista de gêneros</returns>
+        List<GeneroDomain> ListarTodos();
+
+        /// <summary>
+        /// Buscar um objeto através do seu ID
+        /// </summary>
+        /// <param name="id">ID do objeto que sera buscado</param>
+        /// <returns>Objeto que foi buscado</returns>
+        GeneroDomain BuscarPorID(int id);
+
+        /// <summary>
+        /// Atualizar um gênero existente passando o ID pelo corpo da requisição
+        /// </summary>
+        /// <param name="genero">Objeto com as novas informações</param>
+        void AtualizarIdCorpo(GeneroDomain genero);
+
+        /// <summary>
+        /// Atualizar um gênero existente passando o id pela URL da requisição
+        /// </summary>
+        /// <param name="id">ID do objeto a ser atualizado</param>
+        /// <param name="genero">Objeto com as novas informações</param>
+        void AtualizarIdUrl(int id, GeneroDomain genero);
+
+        /// <summary>
+        /// Deletar um gênero existente
+        /// </summary>
+        /// <param name="id">ID do objeto a ser deletado</param>
+        void Deletar(int id);
+    }
+}
