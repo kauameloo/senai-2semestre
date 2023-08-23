79f129a2185bffc5869ffdfc226ae71d06a54ef4
Kauã Rodrigues <kaua.rodrigues2620@gmail.com>
2023-08-23 17:15:11 -0300

    metodo buscarporid

--- api_tarde/webapi.filmes.tarde/Controllers/GeneroController.cs
+++ api_tarde/webapi.filmes.tarde/Controllers/GeneroController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using webapi.filmes.tarde.Domains;
+using webapi.filmes.tarde.Interfaces;
+using webapi.filmes.tarde.Repositories;
 
 namespace webapi.filmes.tarde.Controllers
 {
@@ -8,5 +11,32 @@ namespace webapi.filmes.tarde.Controllers
     [Produces("application/json")]
     public class GeneroController : ControllerBase
     {
+        /// <summary>
+        /// Objeto que irá receber os métodos definidos na interface
+        /// </summary>
+        private IGeneroRepository _generoRepository { get; set; }
+
+        /// <summary>
+        /// Instância do objeto _generoRepository para que haja referência aos metodos do repositório
+        /// </summary>
+        public GeneroController() 
+        { 
+            _generoRepository= new GeneroRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            try
+            {
+                List<GeneroDomain> listaGenero = _generoRepository.ListarTodos();
+
+                return Ok(listaGenero);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
     }
 }
