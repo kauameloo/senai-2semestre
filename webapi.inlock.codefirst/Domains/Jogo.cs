475671877488f034bae887a6020149ee11e3fc55
Kauã Rodrigues <kaua.rodrigues2620@gmail.com>
2023-09-13 17:19:48 -0300

    inicio code first

---
+++ webapi.inlock.codefirst/Domains/Jogo.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.inlock.codefirst.Domains
+{
+    [Table("Jogo")]
+    public class Jogo
+    {
+        [Key]
+        public Guid IdJogo { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "Nome do jogo obrigatório!")]
+        public string? Nome { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "Descrição obrigatória!")]
+        public string? Descricao { get; set; }
+
+        [Column(TypeName = "DATE")]
+        [Required(ErrorMessage = "Data obrigatória!")]
+        public DateTime DataLancamento { get; set; }
+
+        [Column(TypeName = "DECIMAL(4,2)")]
+        [Required(ErrorMessage = "Preço obrigatório!")]
+        public decimal Preco { get; set; }
+
+        public Guid IdEstudio { get; set; }
+
+        [ForeignKey("IdEstudio")]
+        public Estudio? Estudio { get; set; }
+    }
+}
---
+++ webapi.inlock.codefirst/Domains/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.inlock.codefirst.Domains
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Key]
+        public Guid IdUsuario { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "O email é obrigatório!")]
+        public string? Email { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "A senha é obrigatório!")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "A senha deve conter de 6 a 20 caractéres")]
+        public string? Senha { get; set; }
+    }
+}
